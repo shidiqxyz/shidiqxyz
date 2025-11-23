0ddee61afc44966b9eb10c47227191f89c56481f
Muhamad Shidiq Maulana <shidiqmuh0@gmail.com>
2025-11-24 05:22:46 +0700

    fix: add remark-footnotes plugin for proper production footnote support + add favicon

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,12 +1,13 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 import remarkGfm from 'remark-gfm';
+import remarkFootnotes from 'remark-footnotes';
 
 const config = defineConfig({
 	extensions: ['.md', '.svx'],
 	smartypants: {
 		dashes: 'oldschool'
 	},
-	remarkPlugins: [remarkGfm],
+	remarkPlugins: [remarkGfm, [remarkFootnotes, { inlineNotes: true }]],
 	rehypePlugins: []
 });
 
